5a8e917a8ac4c57cf0b94e45e89059b5838bd965
tuannvph17655 <tuannvph17655@fpt.edu.vn>
2021-11-26 22:47:15 +0700

    FixTen

--- QLCAFE/src/DAO/MaXacNhanDAO.java
+++ QLCAFE/src/DAO/MaXacNhanDAO.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dao;
+package DAO;
 
-import helper.JdbcHelper;
+import Helper.JdbcHelper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
